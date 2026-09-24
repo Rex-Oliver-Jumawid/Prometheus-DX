4984d8f44972c995191d48fca71f18101e09dd5b
Olivero <rogjumawid@mymail.mapua.edu.ph>
2026-09-24 20:01:51 +0800

    Register project chat and announcements

--- server/projects/projects.module.ts
+++ server/projects/projects.module.ts
@@ -7,6 +7,10 @@ import { OutcomeWorkController } from './outcome-work.controller';
 import { OutcomeWorkService } from './outcome-work.service';
 import { OutcomeDeliveryController } from './outcome-delivery.controller';
 import { OutcomeDeliveryService } from './outcome-delivery.service';
+import { ProjectChatController } from './project-chat.controller';
+import { ProjectChatService } from './project-chat.service';
+import { ProjectAnnouncementController } from './project-announcement.controller';
+import { ProjectAnnouncementService } from './project-announcement.service';
 
 @Module({
   controllers: [
@@ -14,12 +18,16 @@ import { OutcomeDeliveryService } from './outcome-delivery.service';
     ProjectWorkflowController,
     OutcomeWorkController,
     OutcomeDeliveryController,
+    ProjectChatController,
+    ProjectAnnouncementController,
   ],
   providers: [
     ProjectsService,
     ProjectWorkflowService,
     OutcomeWorkService,
     OutcomeDeliveryService,
+    ProjectChatService,
+    ProjectAnnouncementService,
   ],
   exports: [ProjectsService],
 })
