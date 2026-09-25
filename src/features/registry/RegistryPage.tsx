c97322276d34470683662673885a94ec82b5dca6
Olivero <rogjumawid@mymail.mapua.edu.ph>
2026-09-26 01:08:14 +0800

    Use member removal contract in Registry UI

--- src/features/registry/RegistryPage.tsx
+++ src/features/registry/RegistryPage.tsx
@@ -6,6 +6,7 @@ import {
   CreateDepartmentRequestSchema,
   CreateMemberRequestSchema,
   DeleteDepartmentResponseSchema,
+  RemoveMemberResponseSchema,
   RegistryDepartmentSchema,
   RegistryMemberSchema,
   UpdateMemberRequestSchema,
