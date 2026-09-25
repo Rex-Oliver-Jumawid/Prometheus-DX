737c206ca01d994f106162e50e7da29f52860e9e
Olivero <rogjumawid@mymail.mapua.edu.ph>
2026-09-25 09:52:22 +0800

    feat: register rate limiter for annotated API routes

--- server/visiwork/visiwork.module.ts
+++ server/visiwork/visiwork.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ApiRateLimitGuard } from '../common/rate-limit';
 import { VisiWorkController } from './visiwork.controller';
 import { VisiWorkService } from './visiwork.service';
 
 @Module({
   controllers: [VisiWorkController],
-  providers: [VisiWorkService],
+  providers: [VisiWorkService, ApiRateLimitGuard],
 })
 export class VisiWorkModule {}
